65b905199c35e6b1ec9db2a2a865b3ed0e58bd76
fazlul-01 <faszhulkareem2000@gmail.com>
2021-03-24 01:59:40 +0530

    final

--- app.py
+++ app.py
@@ -17,14 +17,13 @@ def home():
 def predict_api():
     data = request.get_json(force=True)
     prediction = model.predict(list(data.values()))
-    print(list(data.values())[0])
-    print(prediction)
+    # print(list(data.values())[0])
+    # print(prediction)
     confidence = model.predict_proba([list(data.values())[0]])[:, 1]
-    print()
     return jsonify(result=int(list(prediction)[0]), confidence_percentage=confidence[0]*100)
 
 
 if __name__ == '__main__':
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
